06aab9d20881f41f34c25e568fc71c9624cb0e94
Jay Udall <jayudall@csilvm-15.cs.ucsb.edu>
2024-12-02 14:37:10 -0800

    initial commit for lab08

---
+++ SimpleList.cpp
@@ -0,0 +1,74 @@
+#include "SimpleList.h"
+#include <exception>
+
+template <class T>
+SimpleList<T>::SimpleList() {
+    numElements = 0;
+    elements = new T[CAPACITY];
+}
+
+template <class T>
+SimpleList<T>::~SimpleList() {
+    delete[] elements;
+}
+
+template <class T>
+T SimpleList<T>::at(int index) const {
+    if (index < 0 || index >= numElements) {
+        throw InvalidIndexException();
+    }
+    return elements[index];
+}
+
+template <class T>
+bool SimpleList<T>::empty() const {
+    return (numElements == 0);
+}
+
+template <class T>
+T SimpleList<T>::first() const {
+    if (this->empty()) {
+        throw EmptyListException();
+    }
+    return elements[0];
+}
+
+template <class T>
+T SimpleList<T>::last() const {
+    if (this->empty()) {
+        throw EmptyListException();
+    }
+    return elements[numElements - 1];
+}
+
+template <class T>
+int SimpleList<T>::getNumElements() const {
+    return numElements;
+}
+
+template <class T>
+void SimpleList<T>::insert(T item) {
+    if (numElements == CAPACITY) {
+        throw FullListException();
+    }
+    elements[numElements] = item;
+    numElements++;
+}
+
+template <class T>
+void SimpleList<T>::remove(int index) {
+    if (this->empty()) {
+        throw EmptyListException();
+    }
+    if (index < 0 || index >= CAPACITY) {
+        throw InvalidIndexException();
+    }
+    for (int i = index; i < numElements-1; i++) {
+        elements[i] = elements[i+1];
+    }
+    numElements--;
+}
+
+
+//SimpleList.cpp to be completed by the student
+//
